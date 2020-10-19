51a93e66205efd3320b38420c2111427b59b7823
Gwendolyn99 <73113167+Gwendolyn99@users.noreply.github.com>
2020-10-19 20:38:43 +0800

    Add files via upload

---
+++ 5001-1-3.1.py
@@ -0,0 +1,15 @@
+import xml.dom.minidom
+import re
+
+dom = xml.dom.minidom.parse('/Users/JwPrince/Desktop/HKUST DDM/5001 Introduction to Computational and Modeling Tools/assi/assi1/blocklist.xml')
+root = dom.documentElement
+itemlist = root.getElementsByTagName('emItem')
+for i in range(len(itemlist)):
+    item = itemlist[i]
+    att_value1 = item.getAttribute('blockID')
+    att_value2 = item.getAttribute('id')
+    result = re.match('^[ig]\d+$', att_value1)
+    if result:
+        print(r'<emItem blockID="{}" id="{}">'.format(att_value1,att_value2))
+
+
